b90e93b48d2e52a96fc6e9848b365aa7880be9af
ashen007 <asheniranga@gmail.com>
2023-05-03 10:19:35 +0530

    update requirement file and update docstrings

--- models/ieee.py
+++ models/ieee.py
@@ -5,6 +5,46 @@ import json
 
 
 class IEEE:
+    """
+    Parameters
+    ----------
+    query: str
+        search term for either simple search or advanced search, if for advanced
+        search need to add AND, OR, NOT in between search keywords.
+
+    Attributes
+    ----------
+    headers: dict
+        header to post for IEEE Xplore
+
+    payload: dict
+        additional details for filter results from request
+
+    page_count: int
+        total number of pages in the search results
+
+    links_to_paper: dict
+        mined links and additional details for results
+
+    Methods
+    -------
+    post_request:
+        send request to IEEE server
+
+    check_for_multiple_pages:
+        check weather results has been divide to multiple
+        web pages, if so update the page count.
+
+    mine_links:
+        get links for each document from search results
+
+    get_links_to_papers:
+        add all links to single object
+
+    to_csv:
+        dump links to json file
+
+    """
 
     def __init__(self, query):
         self.headers = {
@@ -24,20 +64,50 @@ class IEEE:
         self.links_to_paper = {}
 
     @staticmethod
-    def post_request(header, json):
+    def post_request(header: dict, json: dict) -> requests.Response:
+        """
+        send request to IEEE server
+
+        Parameters
+        ----------
+        header: dict
+            header to post for IEEE Xplore
+
+        json: dict
+            additional details for filter results from request
+
+        Returns
+        -------
+
+        """
         result = requests.post("https://ieeexplore.ieee.org/rest/search",
                                headers=header,
                                json=json)
 
         return result
 
-    def check_for_multiple_pages(self):
+    def check_for_multiple_pages(self) -> bool:
+        """
+        check weather results has been divide to multiple
+        web pages, if so update the page count.
+
+        Returns
+        -------
+
+        """
         results = self.post_request(self.headers, self.payload).json()
         self.page_count = results['totalPages']
 
         return True if self.page_count > 1 else False
 
-    def mine_links(self):
+    def mine_links(self) -> None:
+        """
+        get links for each document from search results
+
+        Returns
+        -------
+
+        """
         request = self.post_request(self.headers, self.payload)
         j = 1
 
@@ -52,7 +122,14 @@ class IEEE:
                                                             record.get('documentLink', None),
                                                             record.get('publicationYear', None)]
 
-    def get_links_to_papers(self):
+    def get_links_to_papers(self) -> None:
+        """
+        add all links to single object
+
+        Returns
+        -------
+
+        """
         if self.check_for_multiple_pages():
             for i in range(1, (self.page_count + 1)):
                 self.payload["pageNumber"] = i
@@ -64,6 +141,18 @@ class IEEE:
         else:
             self.mine_links()
 
-    def to_csv(self, path):
+    def to_csv(self, path: str) -> None:
+        """
+        dump links to json file
+
+        Parameters
+        ----------
+        path: str
+            string path for save results (link and additional details)
+
+        Returns
+        -------
+
+        """
         with open(path, 'w') as file:
             json.dump(self.links_to_paper, file)
