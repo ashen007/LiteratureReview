c141c508a3a2c47fd5e49a7d6537c922ed04e184
ashen007 <asheniranga@gmail.com>
2023-05-05 13:00:39 +0530

    completed acm scrapper

--- models/acm.py
+++ models/acm.py
@@ -7,82 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 
 
-class ScienceDirect:
-    """
-    Parameters
-    ----------
-    start: int
-        start year of the date range filter
-
-    end: int
-        end year of the date range filter
-
-    search_terms: str
-        string of search terms (it can be comma seperated or semicolon
-        seperated string)
-
-    Attributes
-    ----------
-    driver: undetected_chromedriver.Chrome
-        web driver for selenium
-
-    page_count: int
-        number of pages in search results
-
-    links_to_paper: dict
-        mined links and additional details for results
-
-    origin: str
-        origin of science direct advanced search url
-
-    date_filter: str
-        date range to filter search results
-
-    results_in_a_page: str
-        number of records should show tin single page
-
-    offset: str
-        number of records should go forward for next page
-        in search results
-
-    query_text: str
-        encoded search query string to apply in URL
-
-    article_type: str
-        science direct article type category indicator
-
-    Methods
-    -------
-    encode_search_terms_into_query:
-        encode user given search terms into URL string
-
-    construct_full_link:
-        create full link to make request from server
-
-    init_driver:
-        initiate web driver and session
-
-    close_driver:
-        close web driver and session
-
-    post_request:
-        post a request to science direct server
-
-    check_for_multiple_pages:
-        check weather search results contains multiple pages
-        in results
-
-    mine_links:
-        get links to each search result (for each individual paper)
-
-    get_links_to_papers:
-        create paper link list
-
-    to_csv:
-        dump results into json
-
-    """
-
+class ACM:
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -90,16 +15,21 @@ class ScienceDirect:
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.binary_location = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
 
-    def __init__(self, start, end, search_terms):
+    def __init__(self,
+                 start,
+                 end,
+                 search_terms):
         self.driver = None
         self.page_count = None
-        self.links_to_paper = {}
-        self.origin = "https://www.sciencedirect.com/search"
-        self.date_filter = f"?date={start}-{end}"
-        self.results_in_a_page = "&show=100"
-        self.offset = "&offset=0"
-        self.query_text = self.encode_search_terms_into_query(search_terms)
-        self.article_type = "&articleTypes=FLA"
+        self.links_to_paper = []
+        self.search_terms = search_terms
+        self.origin = "https://dl.acm.org/action/doSearch?"
+        self.quick_search = "fillQuickSearch=false"
+        self.target = "&target=advanced&expand=dl"
+        self.date_filter = f"&AfterYear={start}&BeforeYear={end}"
+        self.query_text = self.create_query_text()
+        self.start_page = "&startPage=0"
+        self.results_in_a_page = "&pageSize=50"
 
     @staticmethod
     def encode_search_terms_into_query(keywords):
@@ -115,11 +45,24 @@ class ScienceDirect:
         -------
 
         """
-        encode = keywords.replace(' ', "%20")
+        encode = keywords.replace(' ', "+")
         encode = encode.replace(';', "%3B")
+        encode = encode.replace(':', "%3A")
         encode = encode.replace(',', "%2C")
+        encode = encode.replace('(', "%28")
+        encode = encode.replace(')', "%29")
 
-        return f"&qs={encode}"
+        return encode
+
+    def create_query_text(self):
+        """
+        create query text
+
+        Returns
+        -------
+
+        """
+        return f"&AllField={self.encode_search_terms_into_query(self.search_terms)}"
 
     def construct_full_link(self):
         """
@@ -130,11 +73,12 @@ class ScienceDirect:
 
         """
         return ''.join([self.origin,
+                        self.quick_search,
+                        self.target,
                         self.date_filter,
                         self.query_text,
-                        self.results_in_a_page,
-                        self.offset,
-                        self.article_type])
+                        self.start_page,
+                        self.results_in_a_page])
 
     def init_driver(self):
         """
@@ -196,8 +140,9 @@ class ScienceDirect:
         self.post_request(link)
 
         tot_results = int(self.driver.find_element(By.CLASS_NAME,
-                                                   value="search-body-results-text").text.split(' ')[0])
-        self.page_count = int(np.round(tot_results / 100))
+                                                   value="result__count").text.split(' ')[0])
+
+        self.page_count = int(np.round(tot_results / 50))
 
         self.close_driver()
 
@@ -211,11 +156,17 @@ class ScienceDirect:
         -------
 
         """
-        for title, article in zip(self.driver.find_elements(By.CLASS_NAME, value="result-list-title-link"),
-                                  self.driver.find_elements(By.CLASS_NAME, value="article-type")):
-            self.links_to_paper[title.get_attribute('id')] = [title.text,
-                                                              title.get_attribute('href'),
-                                                              article.text]
+        types = self.driver.find_elements(By.CLASS_NAME, value="issue-heading")
+        dates = self.driver.find_elements(By.CLASS_NAME, value="bookPubDate")
+        titles = self.driver.find_elements(By.CLASS_NAME, value="issue-item__title")
+        links = self.driver.find_elements(By.CSS_SELECTOR,
+                                          value="h5[class='issue-item__title']>span[class='hlFld-Title']>a")
+
+        for type_, date, title, link in zip(types, dates, titles, links):
+            self.links_to_paper.append({"type_": type_.text,
+                                        "date": date.text,
+                                        "title": title.text,
+                                        "link": link.get_attribute('href')})
 
         time.sleep(np.random.uniform(2, 4))
 
@@ -228,8 +179,8 @@ class ScienceDirect:
 
         """
         if self.check_for_multiple_pages():
-            for i in range(self.page_count):
-                self.offset = f"&offset={100 * i}"
+            for i in range(1, (self.page_count + 1)):
+                self.start_page = f"&startPage={i}"
                 self.init_driver()
                 self.post_request(self.construct_full_link())
                 self.mine_links()
